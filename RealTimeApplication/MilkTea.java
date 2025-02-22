b25cc4d7d52d4d95cbbe23758126287000ee343c
Vaibhav Devkar <mrvd6966@gmail.com>
2025-02-22 08:37:11 +0530

    Create MilkTea.java

---
+++ RealTimeApplication/MilkTea.java
@@ -0,0 +1,24 @@
+package RealTimeApplication;
+
+public class MilkTea implements Tea{
+
+	@Override
+	public void making() {
+		// TODO Auto-generated method stub
+		System.out.println("Process to make balck tea");
+		System.out.println("Take some water");
+		System.out.println("Take some Sugar");
+		System.out.println("Take some tea powder");
+		System.out.println("Add some Milk");
+		System.out.println("start gas and wait for some time");
+		System.out.println("Milk tea is ready....!");
+	}
+
+	@Override
+	public void benefits() {
+		// TODO Auto-generated method stub
+		System.out.println("Milk tea is good to refreh the mood");
+		
+	}
+	
+}
