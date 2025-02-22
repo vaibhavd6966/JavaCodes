35b2d848384533a152f3e63903513d1937225aff
Vaibhav Devkar <mrvd6966@gmail.com>
2025-02-22 08:17:27 +0530

    Create BlackTea.java

---
+++ RealTimeApplication/BlackTea.java
@@ -0,0 +1,23 @@
+package RealTimeApplication;
+
+public class BlackTea implements Tea{
+
+	@Override
+	public void making() {
+		// TODO Auto-generated method stub
+		System.out.println("Process to make balck tea");
+		System.out.println("Take some water");
+		System.out.println("Take some Sugar");
+		System.out.println("Take some tea powder");
+		System.out.println("start gas and wait for some time");
+		System.out.println("Black tea is ready....!");
+	}
+
+	@Override
+	public void benefits() {
+		// TODO Auto-generated method stub
+		System.out.println("Black tea is good to health and reduce body temp");
+		
+	}
+	
+}
